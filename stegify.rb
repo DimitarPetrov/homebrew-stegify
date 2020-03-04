d203f725be1648714229f7d1793d7ea35970e991
Dimitar Petrov <buldozer9993@gmail.com>
2020-03-04 19:40:20 +0200

    Update stegify.rb

--- stegify.rb
+++ stegify.rb
@@ -1,9 +1,9 @@
 class Stegify < Formula
   desc "Tool for LSB steganography, capable of hiding any file within an image.
-  version "1.2"
-  bottle :unneeded
 
   url "https://github.com/DimitarPetrov/stegify/releases/download/v1.2/stegify_osx"
+  sha256 "a38e554d484e5c9249977d35729e164fe163eb048e7c85ec2a3bc79edcde6510"
+  
   system "mv" "stegify_osx" "stegify"
 
   def install
