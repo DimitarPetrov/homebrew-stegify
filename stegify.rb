f9ec0d552179f577dfd1e78b8dc17d7481f84d4e
Dimitar Petrov <buldozer9993@gmail.com>
2020-03-04 19:17:20 +0200

    Create stegify.rb

---
+++ stegify.rb
@@ -0,0 +1,11 @@
+class Stegify < Formula
+  desc "Tool for LSB steganography, capable of hiding any file within an image.
+  version "1.2"
+  bottle :unneeded
+
+  url "https://github.com/DimitarPetrov/stegify/releases/download/v1.2/stegify_osx"
+
+  def install
+    bin.install "stegify"
+  end
+end
